8190d4bf328db7a1bad5c5389cee1cb0d3d646e0
itsjordanmuller <itsjordanmuller@gmail.com>
2024-02-20 22:28:02 -0800

    Add Day 72, The Rings HTML, CSS and JS

---
+++ 072-The-Rings/script.js
@@ -0,0 +1,23 @@
+const style = document.createElement("style");
+document.head.appendChild(style);
+
+const middle = document.querySelector(".middle");
+
+for (let i = 1; i <= 10; i++) {
+  const circle = document.createElement("div");
+  circle.classList.add("circle", `circle-${i}`);
+  middle.appendChild(circle);
+
+  circle.style.animation = `rotate-${i} 5s ease-in-out infinite`;
+
+  const keyframes = `@keyframes rotate-${i} {
+    ${i * 4}% {
+      transform: rotateX(0) rotateY(0) rotateZ(0);
+    }
+    100% {
+      transform: rotateX(360deg) rotateY(-360deg) rotateZ(0);
+    }
+  }`;
+
+  style.appendChild(document.createTextNode(keyframes));
+}

cfd75e9031b52913601fb5b4662fab02c92cb395
itsjordanmuller <itsjordanmuller@gmail.com>
2024-03-05 12:18:48 -0800

    Refactor and improve readability of Day 31

--- initial-solutions/031-Pendulum-Wave/script.js
+++ initial-solutions/031-Pendulum-Wave/script.js
@@ -1,8 +1,8 @@
-const circleHolder = document.getElementById("circleHolder");
+const dotArea = document.querySelector(".dot-area");
 
 for (let i = 1; i <= 20; i++) {
   let dot = document.createElement("div");
   dot.classList.add("dot");
   dot.style.setProperty("--i", i);
-  circleHolder.appendChild(dot);
+  dotArea.appendChild(dot);
 }

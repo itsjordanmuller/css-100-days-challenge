81be4b3cdda2f22d75a0f0ecae9a845550be57ca
itsjordanmuller <itsjordanmuller@gmail.com>
2024-03-06 10:53:18 -0800

    Refactor and improve readability of Day 44

--- initial-solutions/044-Twisted-Pyramid/script.js
+++ initial-solutions/044-Twisted-Pyramid/script.js
@@ -1,9 +1,33 @@
-const container = document.querySelector(".container");
+document.addEventListener("DOMContentLoaded", function () {
+  const frame = document.querySelector(".frame");
+  const rects = [];
 
-for (let i = 1; i <= 20; i++) {
-  const div = document.createElement("div");
+  for (let i = 1; i <= 20; i++) {
+    const div = document.createElement("div");
+    const animationDelay = i * 0.03333;
 
-  div.className = `rect rect-${i}`;
+    div.className = `rect rect-${i}`;
+    div.style.width = `${210 - i * 10}px`;
+    div.style.height = `${210 - i * 10}px`;
+    div.style.top = `${100 + i * 5}px`;
+    div.style.left = `${100 + i * 5}px`;
+    div.style.borderColor = `rgb(${78 + i * 8}, ${78 + i * 8}, ${78 + i * 8})`;
+    div.style.zIndex = `${i}`;
+    div.style.animation = `rotate 2s ease-in-out ${animationDelay}s alternate infinite`;
 
-  container.appendChild(div);
-}
+    frame.appendChild(div);
+    rects.push(div);
+  }
+
+  frame.addEventListener("mouseenter", () => {
+    for (const rect of rects) {
+      rect.style.animationPlayState = "paused";
+    }
+  });
+
+  frame.addEventListener("mouseleave", () => {
+    for (const rect of rects) {
+      rect.style.animationPlayState = "running";
+    }
+  });
+});
